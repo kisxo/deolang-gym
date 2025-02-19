7b13e5a16443c4ac0a106fffa72dea26062d00f9
uday <udaysubba2004@gmail.com>
2025-02-19 12:01:58 +0530

    init Gym management fastapi backend

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Gym Management API - DEOLANG",
+    summary="This is the documentation of Gym Management API developed by Deolang. It is a JSON based RESTful API with custom endpoints to streamline gym management.",
+    contact={
+        "name": "Email- Uday Subba",
+        "email": "udaysubba2004@gmail.com",
+    },
+    servers=[
+        {"url": "/api/v1", "description": "Default api URL Route"},
+    ],
+    root_path="/api/v1",
+)
+
+@app.get("/",
+    name= "Api Status",
+    description= """
+        Root route to check the status of the Backend.
+            """,
+    tags=["Root"]
+         )
+async def root():
+    return {"status":"true", "message": "Api server is up and running"}
