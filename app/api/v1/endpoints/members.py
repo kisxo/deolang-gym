c4961479b2c50ff4a36f0cc19a5249bbdda15573
uday <udaysubba2004@gmail.com>
2025-02-21 18:45:40 +0530

    add list members list route

--- app/api/v1/endpoints/members.py
+++ app/api/v1/endpoints/members.py
@@ -1,7 +1,25 @@
 from fastapi import APIRouter
+from app.db.session import SessionDep
+from app.db.models.member import Members
+from app.db.schemas.member import MembersPublic
+from sqlalchemy import select
 
 router = APIRouter()
 
-@router.get("/")
-async def list_members():
-    return {"status":"true", "message": "Members"}
+@router.get("/",
+    response_model=MembersPublic
+)
+async def list_members(session: SessionDep):
+    """
+    Returns a list of member object.\n
+    Each member object contains
+    1. Member id
+    2. Member name
+    3. member gender
+    4. Member phone
+    """
+    statement = select(Members.member_id, Members.member_name, Members.member_gender, Members.member_phone)
+    # used mapping to resolve type-error caused due to use of enums
+    result = session.execute(statement).mappings().all()
+
+    return {'data': result}
