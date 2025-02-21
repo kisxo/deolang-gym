29f222e274839ad881134fa83f746d11c27e1d2e
uday <udaysubba2004@gmail.com>
2025-02-21 20:52:59 +0530

    add JWT signing key

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,10 @@ class Settings(BaseSettings):
     POSTGRES_PORT: str
     POSTGRES_DB: str
 
+    #JWT Signing keys
+    JWT_PRIVATE: str
+    JWT_PUBLIC: str
+
     model_config = SettingsConfigDict(
         env_file=".env",
     )
