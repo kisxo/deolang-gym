22a92615915899a3cfe2db262f6d8f2b2656e8c5
uday <udaysubba2004@gmail.com>
2025-02-21 18:44:49 +0530

    add Members response model and update enum to show value

--- app/db/schemas/member.py
+++ app/db/schemas/member.py
@@ -1,20 +1,29 @@
 import enum
+from pydantic import BaseModel
 
 class GenderChoice(enum.Enum):
     male = "Male"
     female = "Female"
+    def __str__(self) -> str:
+        return self.value
 
 class TrainingChoice(enum.Enum):
     trainer = "Trainer"
     personal = "Personal"
+    def __str__(self) -> str:
+        return self.value
 
 class PlanChoice(enum.Enum):
     regular = "Regular"
     advance = "Advance"
+    def __str__(self) -> str:
+        return self.value
 
 class BatchChoice(enum.Enum):
     morning = "Morning"
     evening = "Evening"
+    def __str__(self) -> str:
+        return self.value
 
 class StatusChoice(enum.Enum):
     active = "Active"
@@ -25,3 +34,14 @@ class StatusChoice(enum.Enum):
     cancelled = "Cancelled"
     pending = "Pending"
     hold = "Hold"
+    def __str__(self) -> str:
+        return self.value
+
+class MemberPublic(BaseModel):
+    member_id: int
+    member_name: str
+    member_gender: GenderChoice
+    member_phone: str
+
+class MembersPublic(BaseModel):
+    data: list[MemberPublic]
--- app/db/schemas/payment.py
+++ app/db/schemas/payment.py
@@ -3,3 +3,5 @@ import enum
 class PaymentMethodChoice(enum.Enum):
     cash = "Cash"
     online = "Online"
+    def __str__(self) -> str:
+        return self.value
