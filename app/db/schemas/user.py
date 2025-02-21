12a3ed7b79281acb5cb598359b55c192d272dfba
uday <udaysubba2004@gmail.com>
2025-02-21 22:48:22 +0530

    add user data validation

--- app/db/schemas/user.py
+++ app/db/schemas/user.py
@@ -1,4 +1,6 @@
 import enum
+from pydantic import BaseModel, Field, model_validator
+from typing_extensions import Self
 
 class UserRole(enum.Enum):
     admin = "Admin"
@@ -6,3 +8,20 @@ class UserRole(enum.Enum):
     member = "Member"
     def __str__(self) -> str:
         return self.value
+
+class UserBase(BaseModel):
+    user_username: str = Field(min_length=4, max_length=20)
+
+    @model_validator(mode='after')
+    def check_username(self) -> Self:
+        if self.user_username.isalpha():
+            return self
+        else:
+            raise ValueError('Username should only contain alphabets and numbers!')
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=6)
+    user_role: UserRole = Field(default="Member")
+
+class UserPublic(UserBase):
+    user_role: UserRole
