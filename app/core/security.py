d1ae436ce4fc0ec2fea81d0411680b6baf07200d
uday <udaysubba2004@gmail.com>
2025-02-21 21:12:05 +0530

    add password hashing utility

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,6 @@
 from authx import AuthX, AuthXConfig
 from app.core.config import settings
+import bcrypt
 
 security_config = AuthXConfig(
      JWT_ALGORITHM = "RS256",
@@ -9,3 +10,15 @@ security_config = AuthXConfig(
 )
 
 security = AuthX(config=security_config)
+
+# Hash a password using bcrypt
+def hash_password(password):
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
+    return hashed_password
+
+# Check if the provided password matches the stored hashed_password
+def verify_password(plain_password, hashed_password):
+    password_byte_enc = plain_password.encode('utf-8')
+    return bcrypt.checkpw(password = password_byte_enc , hashed_password = hashed_password)
